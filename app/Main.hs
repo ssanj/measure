f28635dd894850039fcbfe792222b77723bcd924
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-02-24 17:44:14 +1100

    Make it more polymorphic

--- app/Main.hs
+++ app/Main.hs
@@ -21,9 +21,9 @@ import Formatting.Clock
 
 import qualified Data.Version as DV
 
-data Measurement a b c = StartTime a | Running c | EndTime a | TimeTaken b b | Completed ExitCode
+data Measurement a b c d = StartTime a | Running c | EndTime a | TimeTaken b b | Completed ExitCode
 
-type MonotonicMeasurement = Measurement UTCTime TimeSpec String
+type MonotonicMeasurement = Measurement UTCTime TimeSpec String String
 
 main, showHelp, showVersion, showBanner, showCombinedVersion, showCombinedHelp :: IO ()
 
@@ -53,7 +53,7 @@ main = do
     ["-v"]        -> showVersion
     ["--help"]    -> showHelp
     ["-h"]        -> showHelp
-    params@(_:_)    -> runMeasure (intercalate " " params) prettyMeasurement
+    params@(_:_)    -> runMeasure (intercalate " " params) (runProgram prettyMeasurement) prettyMeasurement
     []            -> showBanner >> showCombinedVersion >> showCombinedHelp
 
 
@@ -81,20 +81,20 @@ class Console m a where
 instance Console IO String where
   printLine = putStrLn
 
-runMeasure :: forall f a b c. (Monad f, WallClock f a, MonotonicClock f b, Process f c, Console f c) => c -> (Measurement a b c -> c) -> f ()
-runMeasure program pretty = do
-  startPoint :: b <- getMonotonicTime
-  start :: a      <- getWallTime
+runMeasure :: forall f a b c d. (Monad f, WallClock f a, MonotonicClock f b, Process f c, Console f d) => c -> (c -> f()) -> (Measurement a b c d -> d) -> f ()
+runMeasure program programRunner pretty = do
+  startPoint <- getMonotonicTime
+  start      <- getWallTime
   let startTime = pretty (StartTime start)
   printLine startTime -- add start time to the top in case the command never completes
-  runProgram pretty program
-  end :: a      <- getWallTime
-  endPoint :: b <- getMonotonicTime
+  programRunner program
+  end      <- getWallTime
+  endPoint <- getMonotonicTime
   printLine startTime -- duplicate start time at the bottom where it's easier to see 
-  printLine $ pretty (EndTime end :: Measurement a b c)
-  printLine $ pretty (TimeTaken startPoint endPoint :: Measurement a b c)
+  printLine $ pretty (EndTime end)
+  printLine $ pretty (TimeTaken startPoint endPoint)
 
-runProgram :: forall f a b c. (Monad f, Process f c, Console f c) => (Measurement a b c -> c) -> c -> f ()
+runProgram :: forall f a b c d. (Monad f, Process f c, Console f d) => (Measurement a b c d -> d) -> c -> f ()
 runProgram pretty program = do 
   printLine $ pretty (Running program)
   exitCode <- runSyncCommand program
